71274f3d3c20d8fac04a1a63dff1651ba8510706
Vadym <vadumprotsyuk@gmail.com>
2024-04-03 16:00:06 +0300

    onLike

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
   const data = [
-    {name: 'John S.',salary: 800, increase: false, id: 1},
-    {name: 'Alex M',salary: 3000, increase: true, id: 2},
-    {name: 'Carl W.',salary: 15000, increase: false, id: 3},
+    {name: 'John S.',salary: 800, increase: false, id: 1, like: false},
+    {name: 'Alex M',salary: 3000, increase: true, id: 2, like: true},
+    {name: 'Carl W.',salary: 15000, increase: false, id: 3, like: false},
 
   ]
 
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -5,7 +5,8 @@ class EmployeesListItem extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            increase: false
+            increase: false,
+            like: false
         }
     }
 
@@ -15,10 +16,17 @@ class EmployeesListItem extends Component{
         }))
     }
 
+    onLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
     render() {
         const {name, salary} = this.props;
         
-        const {increase} = this.state;
+        const {increase, like} = this.state;
+
 
         let classNames = "list-group-item d-flex justify-content-between";
 
@@ -26,9 +34,14 @@ class EmployeesListItem extends Component{
             classNames += ' increase';
         }
 
+        if (like) {
+            classNames += ' like';
+        }
+
         return (
             <li className={classNames}>
-                <span className="list-group-item-label">{name}</span>
+                <span className="list-group-item-label"
+                onClick={this.onLike}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
